71092a080a080f3f2b123d2c693032e980879f30
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2022-12-28 22:31:35 +0200

    Added file dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { open } from '@tauri-apps/api/dialog'
+import { createEffect, createResource } from 'solid-js'
+
+const getPath = async () => {
+	const path = await open({
+		directory: true,
+		multiple: false
+	})
+
+	return path
+}
+
 function App() {
-	return (<></>)
+	const [path, { refetch }] = createResource(getPath)
+
+	createEffect(() => {
+		const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'o' && (e.metaKey || e.ctrlKey)) {
+				refetch()
+			}
+    }
+
+    window.addEventListener('keydown', onKeyDown)
+    return () => {
+      window.removeEventListener('keydown', onKeyDown)
+    }
+	})
+	
+	return (
+		<div>
+			<h1>{ path() }</h1>
+		</div>
+	)
 }
 
 export default App
